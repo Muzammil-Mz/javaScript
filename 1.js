10e57972779768878c671e9427ea85b7fc0a22d5
muzammil-mz <mujjuimran74@gmail.com>
2025-04-10 10:40:01 +0000

    initial

---
+++ 1.js
@@ -0,0 +1,11 @@
+// let a=10
+// let b=20
+// let c=30
+
+console.log({a,b,c}); //gives output in object notation { a: 10, b: 20, c: 30 }
+
+let a=10
+let b=20
+let c=30
+
+console.log(a,b,c); //10 20 30
